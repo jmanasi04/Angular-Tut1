3137a8ffa8709288cf2cc6374f792b479150d528
Manasi Jahagirdar <manasi.jahagirdar@dedalus.com>
2021-10-28 17:15:17 +0200

    Use of directives and introduction to services

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -4,8 +4,17 @@ import {Component} from "@angular/core";
 @Component({
   //One or more properties as arguments so that angular knows how this component works. One such property is 'Selectors'
   selector: 'courses', //element referenced: 'courses'; element with a specific class refrenced: '.courses'; element with a specific id refrenced: '#courses';
-  template:'<h2>Courses</h2>' //contains HTML markup to be rendered for this component
+  //contains HTML markup to be rendered for this component
+  template: `
+    <h2>{{title}}</h2>
+    <ul>
+      <li *ngFor="let course of courses">
+        {{ course }}
+      </li>
+    </ul>
+  `
 })
 export class CoursesComponent {
-
+  title = "List of courses";
+  courses = ;
 }
