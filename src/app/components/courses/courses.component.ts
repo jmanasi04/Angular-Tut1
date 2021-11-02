6fb8cb01b890929d2bbdc26495db787952dca3f8
Manasi Jahagirdar <manasi.jahagirdar@dedalus.com>
2021-11-02 17:32:41 +0100

    Add bootstrap

--- src/app/components/courses/courses.component.ts
+++ src/app/components/courses/courses.component.ts
@@ -6,7 +6,17 @@ import {CoursesService} from "../../services/courses.service";
   //One or more properties as arguments so that angular knows how this component works. One such property is 'Selectors'
   selector: 'courses', //element referenced: 'courses'; element with a specific class refrenced: '.courses'; element with a specific id refrenced: '#courses';
   //contains HTML markup to be rendered for this component
+  //'{{}}' also known as string interpolation syntax
+  //'[]': Component to DOM => property binding
   template: `
+    <img src = "{{imageUrl}}"/>
+    <img [src] = "imageUrl"/>
+    <table>
+      <tr>
+        <td [attr.colspan]="colSpan"></td>
+      </tr>
+    </table>
+    <button class = 'btn btn-primary'>Save</button>
     <h2>{{title}}</h2>
     <ul>
       <li *ngFor="let course of courses">
@@ -18,6 +28,8 @@ import {CoursesService} from "../../services/courses.service";
 export class CoursesComponent {
   title = "List of courses";
   courses;
+  imageUrl = "http://lorempixel.com/400/200";
+  colSpan = 2;
 
   constructor(coursesService: CoursesService) {
     this.courses = coursesService.getCourses();
